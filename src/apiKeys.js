51c354705af1691d461eac9eff7ebda611f365bf
Amirmoeed <amirmoeed1234@gmail.com>
2024-03-16 20:22:43 +0500

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "8422fbde02309a600abbe1ca432d934c",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
